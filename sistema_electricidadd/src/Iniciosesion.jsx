41a109c774cc7c9e4c9465640a80f51c921a91c5
anabellfabian <anabellfabian29@gmail.com>
2025-04-25 10:47:24 -0400

    ...

---
+++ sistema_electricidadd/src/Iniciosesion.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+function Iniciosesion() {
+
+    return (
+    <>
+    <div>
+
+       <h1>Iniciar sesión</h1>
+       <form>
+        
+       </form>
+
+    </div>
+
+    </>
+    )
+}
+
+export default Iniciosesion
--- sistema_electricidadd/src/main.jsx
+++ sistema_electricidadd/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Iniciosesion from './Iniciosesion.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Iniciosesion/>
   </StrictMode>,
+ 
 )
